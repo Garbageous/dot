5e8d076a30d91d1e53d99a0fe15af0b7c701a5d0
Robert Talos <robert.talos@hogia.se>
2024-04-03 12:05:41 +0200

    Refactor readme (#3)
    * rework readme
    
    * added new plugins
    
    * added new plugins

--- lua/base/keymaps.lua
+++ lua/base/keymaps.lua
@@ -33,6 +33,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Restart buffer
+--
+vim.keymap.set('n', '<leader>f', '<cmd>edit!<cr><Esc>', { desc = 'Move focus to the upper window' })
+
 -- Compile mappings
 --
 -- Open compiler
---
+++ lua/custom/plugins/overload.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    'Issafalcon/lsp-overloads.nvim',
+  },
+}
